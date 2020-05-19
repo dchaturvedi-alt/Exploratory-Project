106afcfb0238129d55ca4fea91e9cc2af19c70dd
Divyansh Chaturvedi <thed1813@gmail.com>
2020-05-19 18:49:17 +0530

    Add files via upload

---
+++ GA_TSP/README.md
@@ -0,0 +1,8 @@
+
+Instructions:
+
+ Navigate to the src folder and execute:
+    ```bash
+    cd src
+    python main.py -v 1 --pop_size 500 --tourn_size 50 --mut_rate 0.02 --n_gen 20 --cities_fn '../../data/rat99.txt'
+    ```
---
+++ GA_TSP/src/main.py
@@ -0,0 +1,56 @@
+import utils
+import random
+import argparse
+import tsp_ga as ga
+from datetime import datetime
+
+def run(args):
+    genes = utils.get_genes_from(args.cities_fn)
+
+    if args.verbose:
+        print("-- Running TSP-GA with {} cities --".format(len(genes)))
+
+    history = ga.run_ga(genes, args.pop_size, args.n_gen,
+                        args.tourn_size, args.mut_rate, args.verbose)
+    print(ga.cost_list)
+
+    if args.verbose:
+        print("-- Drawing Route --")
+
+    # utils.plot(history['cost'], history['route'])
+
+    if args.verbose:
+        print("-- Done --")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-v', '--verbose', type=int, default=1)
+    parser.add_argument('--pop_size', type=int, default=500, help='Population size')
+    parser.add_argument('--tourn_size', type=int, default=50, help='Tournament size')
+    parser.add_argument('--mut_rate', type=float, default=0.02, help='Mutation rate')
+    parser.add_argument('--n_gen', type=int, default=20, help='Number of equal generations before stopping')
+    parser.add_argument('--cities_fn', type=str, default="../../data/rat99.txt", help='Data containing the geographical coordinates of cities')
+
+    random.seed(datetime.now())
+    args = parser.parse_args()
+
+    if args.tourn_size > args.pop_size:
+        raise argparse.ArgumentTypeError('Tournament size cannot be bigger than population size.')
+
+
+    # nInstance = len(ga.cost_list)
+    # Best_cost = max(ga.cost_list)
+    # Average_cost = sum(ga.cost_list)/nInstance
+    # Deviation = Best_cost-Average_cost
+
+    # f = open("../data/output.txt","a")
+    # print(args.pop_size)
+    # f.write("\nGA Parameters: Population Size: "+str(args.pop_size)+" Tournament size: "+str(args.tourn_size)+" Mutation Rate: "+str(args.mut_rate)+" Number of genes: "+str(args.n_gen)
+        # +"\n")
+    # f.write("\t"+"Instance"+"\t"+"Best cost"+"\t"+"Average cost"+"\t"+"Deviation"+"\t"+"nInstance"+"\n")
+    # f.write("\t"+"rat99"+"\t"+Best_cost+"\t"+Average_cost+"\t"+Deviation+"\t"+nInstance+"\n")
+    # f.close()
+
+    run(args)
